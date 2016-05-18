299fcffff572e9d2eb7c36c77f35e00e70c8269d
yuanhai <jethai@126.com>
2016-05-18 18:32:22 +0800

    first commit

---
+++ src/cn/itcast/student/MainActivity.java
@@ -0,0 +1,62 @@
+package cn.itcast.student;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    private EditText studentno;
+    private ServiceConnection conn = new StudentServiceConnection();
+    private IStundent iStundent;
+    private TextView resultView;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+           
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        resultView = (TextView) this.findViewById(R.id.resultView);
+        
+ /*       	View view = View.inflate(this, R.layout.main, null);
+    	Button inflateButton = (Button) view.findViewById(R.id.button);*/
+        
+        studentno = (EditText) this.findViewById(R.id.studentno);
+        Button button = (Button) this.findViewById(R.id.button);
+        button.setOnClickListener(new ButtonClickListener());
+        Intent service = new Intent(this, StudentService.class);
+        bindService(service, conn, BIND_AUTO_CREATE);
+        System.out.println(Thread.currentThread().getName());
+    }
+    
+    private class StudentServiceConnection implements ServiceConnection{
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			iStundent = (IStundent)service;
+		}
+		public void onServiceDisconnected(ComponentName name) {
+			iStundent = null;
+		}
+    }
+    
+    @Override
+	protected void onDestroy() {
+		unbindService(conn);
+		super.onDestroy();
+	}
+
+	private final class ButtonClickListener implements View.OnClickListener{
+		public void onClick(View v) {
+			 
+			
+			System.out.println(Thread.currentThread().getName());
+			String no = studentno.getText().toString();
+			String name = iStundent.queryStudent(Integer.valueOf(no));
+			resultView.setText(name);
+		}
+    }
+}
---
+++ src/cn/itcast/student/StudentService.java
@@ -0,0 +1,49 @@
+package cn.itcast.student;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class StudentService extends Service{
+	private String[] names = {"张飞","李小龙","赵薇"};
+	private IBinder binder = new StundentBinder();
+	
+	public String query(int no){
+		
+	/*	  try {
+	   			//超过5s会报 ANR (“Application Not Responding”)错误
+	   			//同一个apk下面的service和Activity没有特殊的情况下，一般是在同一个进程的同一个线程里面的。
+	   			//都在主UI线程，这就是为什么我们在service的onstart onbind中不能做延时较长的操作。 
+	   			Thread.sleep(20000);
+	   		} catch (InterruptedException e) {
+	   			e.printStackTrace();
+	   		}
+		  */
+		if(no>0 && no<4){
+	System.out.println(Thread.currentThread().getName());
+			return names[no - 1];
+		}
+		return null;
+	}
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		/*try {
+			//超过5s会报 ANR (“Application Not Responding”)错误
+			//同一个apk下面的service和Activity没有特殊的情况下，一般是在同一个进程的同一个线程里面的。
+			//都在主UI线程，这就是为什么我们在service的onstart onbind中不能做延时较长的操作。 
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}*/
+		return binder;
+	}
+	
+	private class StundentBinder extends Binder implements IStundent{
+		public String queryStudent(int no) {
+			return query(no);
+		}
+	}
+
+}
